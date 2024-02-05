1533ac468cbe2bfaa0b50889da9cc8ffce97c327
ammaar <muhammadfarroos12@gmail.com>
2024-02-05 11:43:10 +0700

    stater login page

---
+++ lib/presentation/auth/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pos/components/buttons.dart';
+import 'package:flutter_pos/presentation/home/pages/dashboard_page.dart';
+
+import '../../../components/custom_text_field.dart';
+import '../../../components/spaces.dart';
+import '../../../core/assets/assets.gen.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          const SpaceHeight(80.0),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 130.0),
+              child: Image.asset(
+                Assets.images.logo.path,
+                width: 100,
+                height: 100,
+              )),
+          const SpaceHeight(24.0),
+          const Center(
+            child: Text(
+              "POS Batch 11",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SpaceHeight(8.0),
+          const Center(
+            child: Text(
+              "Masuk untuk kasir",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          const SpaceHeight(40.0),
+          CustomTextField(
+            controller: usernameController,
+            label: 'Username',
+          ),
+          const SpaceHeight(12.0),
+          CustomTextField(
+            controller: passwordController,
+            label: 'Password',
+            obscureText: true,
+          ),
+          const SpaceHeight(24.0),
+          Button.filled(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=> const DashboardPage()));
+          }, label: 'Masuk')
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/home/pages/dashboard_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('dashboard'),),
+      body: const Center(
+        child: Text('Dashboard'),
+      ),
+    );
+  }
+}
